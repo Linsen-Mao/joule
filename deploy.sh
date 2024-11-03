dad98ce32104d0e1e78984e2944d5b44ca1ff40c
Linsen-Mao <andrew.ls.mao@gmail.com>
2024-11-03 14:57:08 +0800

    update deploy.sh to show secret

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ kubectl apply -f k8s/pgvector-deployment.yaml
 kubectl apply -f k8s/app-deployment.yaml
 
 echo "Checking service status..."
-kubectl get pod,deployment,service
+kubectl get pod,deployment,service,secret
 
 echo "Deployment complete! If running locally, use the following command to port-forward and access the application:"
 echo "kubectl port-forward service/app-service 8080:8080"
