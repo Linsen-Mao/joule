62c1877891f7bc92ada5620efd30f58c618c4c5f
Linsen-Mao <andrew.ls.mao@gmail.com>
2024-11-03 15:35:00 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-echo "Starting application using Docker Compose..."
-docker-compose down
-docker-compose up -d
-
 echo "Deploying Kubernetes configurations..."
 kubectl apply -f k8s/pgvector-deployment.yaml
 kubectl apply -f k8s/app-deployment.yaml
